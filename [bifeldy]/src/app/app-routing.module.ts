14d9db84c9352df1dff56d8c5f3ebcbcc8d7c9d0
Bifeldy <bias.astho@gmail.com>
2019-10-05 21:37:48 +0900

    アップデート

--- [bifeldy]/src/app/app-routing.module.ts
+++ [bifeldy]/src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NewsComponent } from './news/news.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'news',
+    component: NewsComponent
+  },
+  {
+    path: 'timeline',
+    component: TimelineComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- [bifeldy]/src/app/app.module.ts
+++ [bifeldy]/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    NewsComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
