c61bdabab15fdccd10fbee3bdaff1a237c64595b
Bifeldy <bifeldy@gmail.com>
2025-10-11 22:37:16 +0900

    ✔ 11-10-2025

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -43,11 +43,16 @@
                 </v-card-title>
                 <v-card-subtitle>
                   <h3 class="subheading font-weight-regular my-3">
-                    📌 Interest in <b>software</b>, <b>web</b>, <b>data mining</b>, and <b>distributed systems</b> &amp; <b>networks</b><br />
+                    📌 Interest in <b>software</b>, <b>web</b>, <b>data mining</b>, and <b>distributed systems</b> &amp; <b>networks</b>
+                    <br />
                     ✨ TOEIC L&R - ETS :: 765 (Exp. March 19, 2026)
+                    <br /><br />
+                    📃 <a target="_blank" href="https://bifeldy.github.io/cv.html" class="text-decoration-none">View Resume</a>
+                    <br />
+                    🔗 <a target="_self" href="#MyJourney" class="text-decoration-none">My Journey</a>
                   </h3>
                   <v-row class="my-3">
-                    <v-col cols="12" md="6">
+                    <v-col cols="12" md="4">
                       <v-row>
                         <v-col cols="12">
                           <h2 class="subheading font-weight-bold mt-3">
@@ -59,12 +64,6 @@
                             <v-icon>mdi-angular</v-icon>
                           </v-btn>
                           <v-btn icon x-large>
-                            <v-icon>mdi-react</v-icon>
-                          </v-btn>
-                          <v-btn icon x-large>
-                            <v-icon>mdi-vuejs</v-icon>
-                          </v-btn>
-                          <v-btn icon x-large>
                             <v-icon>mdi-android</v-icon>
                           </v-btn>
                         </v-col>
@@ -72,7 +71,7 @@
                       <v-row>
                         <v-col cols="12">
                           <h2 class="subheading font-weight-bold mt-3">
-                            Backend
+                            Backend / Full
                           </h2>
                         </v-col>
                         <v-col cols="12" class="py-0 px-3">
@@ -82,13 +81,10 @@
                           <v-btn icon x-large>
                             <v-icon>mdi-dot-net</v-icon>
                           </v-btn>
-                          <v-btn icon x-large>
-                            <v-icon>mdi-laravel</v-icon>
-                          </v-btn>
                         </v-col>
                       </v-row>
                     </v-col>
-                    <v-col cols="12" md="6">
+                    <v-col cols="12" md="8">
                       <v-row>
                         <v-col cols="12">
                           <h2 class="subheading font-weight-bold mt-3">
@@ -127,9 +123,6 @@
                             <v-icon>mdi-language-csharp</v-icon>
                           </v-btn>
                           <v-btn icon x-large>
-                            <v-icon>mdi-language-php</v-icon>
-                          </v-btn>
-                          <v-btn icon x-large>
                             <v-icon>mdi-language-python</v-icon>
                           </v-btn>
                           <v-btn icon x-large>
@@ -159,7 +152,12 @@
     </v-row>
     <v-row align="center" class="text-center mb-5">
       <v-col cols="12" align="start" class="text-start">
-        <h2>Experience</h2>
+        <h2 class="d-flex justify-space-between flex-row">
+          <span class="text-left">Experience</span>
+          <small class="text-right">
+            <a target="_blank" href="https://www.linkedin.com/in/Bifeldy" class="text-decoration-none">More</a>
+          </small>
+        </h2>
         <v-divider></v-divider>
       </v-col>
       <v-col cols="12" align="start">
@@ -195,7 +193,12 @@
     </v-row>
     <v-row align="center" class="text-center mb-5">
       <v-col cols="12" align="start" class="text-start">
-        <h2>Research &amp; Paper Work</h2>
+        <h2 class="d-flex justify-space-between flex-row">
+          <span class="text-left">Research &amp; Paper Work</span>
+          <small class="text-right">
+            <a target="_blank" href="https://kc.umn.ac.id/cgi/search/archive/advanced?creators_name=Christyono" class="text-decoration-none">More</a>
+          </small>
+        </h2>
         <v-divider></v-divider>
       </v-col>
       <v-col cols="12" align="start">
@@ -224,13 +227,13 @@
       </v-col>
     </v-row>
     <v-row align="center" class="text-center mb-5">
-      <v-col cols="12">
-        <span class="text-primary"> ~ So Bad Yet, So Good! (｡&gt;﹏&lt;｡) ~ </span>
-      </v-col>
-    </v-row>
-    <v-row align="center" class="text-center mb-5">
       <v-col cols="12" align="start" class="text-start">
-        <h2>Featured Hobby</h2>
+        <h2 class="d-flex justify-space-between flex-row">
+          <span class="text-left">Featured Hobby</span>
+          <small class="text-right">
+            <a target="_blank" href="https://github.com/bifeldy?tab=repositories" class="text-decoration-none">More</a>
+          </small>
+        </h2>
         <v-divider></v-divider>
       </v-col>
       <v-col cols="12" align="start">
@@ -252,6 +255,83 @@
             </v-card>
           </v-col>
         </v-row>
+      </v-col>
+    </v-row>
+    <v-row align="center" class="text-center mb-5" id="MyJourney">
+      <v-col cols="12">
+        <span class="text-primary"> ~ So Bad Yet, So Good! (｡&gt;﹏&lt;｡) ~ </span>
+      </v-col>
+    </v-row>
+    <v-row align="center" class="text-center mb-5">
+      <v-col cols="12" align="start" class="text-start">
+        <h2 class="text-left">TL;DR (Long Story) My Journey</h2>
+        <v-divider></v-divider>
+      </v-col>
+      <v-col cols="12" align="start" class="text-justify">
+        <h3 class="my-5">Why computer science</h3>
+        <p style="text-indent: 2em;">
+          I've been familiar with computers since I was a child, when internet cafes and multiplayer games were popular in Indonesia.
+          I was in 6th grade at the time, and I learned programming because I was interested in creating websites to share online game cheats.
+          That's where I learned HTML, JS, VB, and C/C++.
+          In high school, I also enjoyed modifying and customizing my Android devices.
+          My goal remained the same, gaining root access and ensuring my cheats wouldn't be detected when playing games on my Android device.
+        </p>
+        <h3 class="my-5">First time working in the real world</h3>
+        <p style="text-indent: 2em;">
+          In 2019, I interned at PT. Ace Global Consulting & Integrity (a software company under the Indomaret group), 8 to 5, and five days a week.
+          I created and developed applications using Java. 
+          I learned a lot there because it was a new experience for me.
+          I had never used that programming language before, the only programming languages I knew and used daily as a student were C/C++, PHP, and JS.
+        </p>
+        <p style="text-indent: 2em;">
+          After completing my project, I returned to university to continue my studies.
+          I also took a part-time job as a front-end engineer to fill my spare time while completing my thesis.
+        </p>
+        <h3 class="my-5">Graduated from university</h3>
+        <p style="text-indent: 2em;">
+          After graduating in 2020, I returned to the Indomaret group, but this time I was placed in the parent main company, specifically in the logistics department (with a total of 20 programmers), managing Distribution Center (DC) warehouses spread across various cities in Indonesia.
+          Here, we used VB and C# to create various applications, including web, desktop, and mobile, as those were the only programming languages the existing employees were proficient in.
+          This was also a new experience for me, as I had never used them before.
+          We have always used the ASP.NET Core Framework, so most of our servers are Windows-based (we have on-premises servers, not cloud servers or third-party services).
+        </p>
+        <p style="text-indent: 2em;">
+          Initially, my work was limited to building traditional Windows desktop applications the old-fashioned way, where most executable files (app.exe) typically connected directly to the database.
+          Later, we started implementing microservices, and some applications were moved to web pages with REST APIs (while still maintaining the legacy SOAP architecture, as it was still used by other departments).
+        </p>
+        <h3 class="my-5">My First Project</h3>
+        <p style="text-indent: 2em;">
+          Because we were using Windows servers, licensing costs were high, and I was asked to manage and migrate some application services to Linux while still using VB and C#.
+          As a result, some of the servers currently running Windows were downsized.
+          We couldn't completely replace them because we were still using Microsoft SQL Server databases.
+          Furthermore, during this project, I began implementing the concept of application containerization during deployment, a practice that continues to this day.
+          It wasn't a perfect CI/CD pipeline, but it worked, just by utilizing the limited resources we had (physical CPU, RAM, NET, etc.).
+        </p>
+        <h3 class="my-5">The Next, DB-Link Problem</h3>
+        <p style="text-indent: 2em;">
+          Before I joined the company, they were using Clipper DB, which they later migrated to Oracle. 
+          However, after I joined the team, they asked me to migrate to PostgreSQL for cost-effective reasons.
+          I, along with several other people (still considered new employees and expected to be more familiar with the latest technology), began a trial migration.
+          We converted the entire existing table structure, views, functions, procedures, CRON jobs / schedulers, and also created extensions that existed in Oracle but weren't available in PostgreSQL.
+          Fortunately, our servers are decentralized, so migrating a DC won't disrupt the others.
+        </p>
+        <p style="text-indent: 2em;">
+          Oracle has a feature called dblink that makes it easy to retrieve data between database servers, for example, from all branches to the head office (actually, PostgreSQL also has one).
+          However, since we're starting the migration one branch at a time, I had to think about replacing this feature, as it only works for the same database.
+          So, I created a REST API (initially) as a prototype for retrieving data.
+          After testing, I found this method unreliable for sending large amounts of data, so I decided to use Kafka and GRPC for data streaming.
+          This was a side project that eventually evolved into a serious project and became the main foundation for large-scale, real-time data transfers.
+        </p>
+        <h3 class="my-5">What now</h3>
+        <p style="text-indent: 2em;">
+          Currently focusing on user-requested application development and maintenance based on user (logistics) requests or bug fixes in existing programs.
+          There likely won't be any more major infrastructure development projects.
+          Just reading the requirements, write SQL scripts, code in VB / C#, and chill out 😊 (in Indonesian, "🧨 jaga lilin 🧨 / keep the lights on / 🧨" when we have nothing to do).
+        </p>
+      </v-col>
+    </v-row>
+    <v-row align="right" class="text-right my-3">
+      <v-col>
+        <h5>Last Updated :: 11-10-2025</h5>
       </v-col>
     </v-row>
   </v-container>
@@ -314,20 +394,6 @@
           description: 'RSS Feed Crawler :: http://crawl.fansub.id',
           image: 'https://raw.githubusercontent.com/bifeldy/FansubID/master/projects/main-site/src/assets/img/banner/news.png',
           url: null
-        },
-        {
-          name: 'ObsUtilGUI',
-          title: 'Huawei OBS Utility GUI',
-          description: 'Windows XP+ Huawei OBS Up & Down',
-          image: 'https://raw.githubusercontent.com/bifeldy/ObsUtilGUI/main/ObsUtilGUI.png',
-          url: null
-        },
-        {
-          name: 'nginx-ui',
-          title: 'Nginx-UI Dockerize',
-          description: 'NginX-UI Web Config Editor',
-          image: 'https://i.ibb.co/59myNSf/nginx-ui.png',
-          url: 'https://hub.docker.com/r/bifeldy/nginx-ui'
         }
       ],
       papers: [
